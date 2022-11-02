9f155abfa73bd97eacba9676db91e515fac1ec9e
Fisalma Maradita <salmamaradita@gmail.com>
2022-11-03 00:08:02 +0700

    update footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,4 @@
 <footer class="bg-secondary text-white text-center pb-3 mt-5">
     <br />
     <div class="text-center p-4">Copyright © 2022 <strong>Fisalma Maradita</strong></div>
-  </footer>
+    </footer>
